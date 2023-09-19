bf28f6b9f45919e1d49961df887e3c8c3c1364b1
Kastan Day <kastanvday@gmail.com>
2023-09-19 16:24:01 -0700

    New huggingface extractor for Clowder2!

---
+++ single-file-huggingface/single_file_huggingface.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+"""Example extractor based on the clowder code."""
+
+import logging
+from distutils import extension
+from typing import Dict
+
+import numpy as np
+import pyclowder.files
+import ray
+from pyclowder.extractors import Extractor
+from ray.util.queue import Queue
+from scipy.special import softmax
+from transformers import (AutoConfig, AutoModelForSequenceClassification, AutoTokenizer, TFAutoModelForSequenceClassification)
+
+
+def preprocess(single_filepath):
+  """Helper..."""
+  text = ""
+  with open(single_filepath, 'r') as file:
+    text = file.read().replace('\n', '')
+  new_text = []
+  for t in text.split(" "):
+    t = '@user' if t.startswith('@') and len(t) > 1 else t
+    t = 'http' if t.startswith('http') else t
+    new_text.append(t)
+  return " ".join(new_text)
+
+
+class SingleFileHuggingFace(Extractor):
+  """Count the number of characters, words and lines in a text file."""
+
+  def __init__(self):
+    Extractor.__init__(self)
+
+    # add any additional arguments to parser
+    # self.parser.add_argument('--max', '-m', type=int, nargs='?', default=-1,
+    #                          help='maximum number (default=-1)')
+
+    # parse command line and load default logging configuration
+    self.setup()
+
+    # setup logging for the exctractor
+    logging.getLogger('pyclowder').setLevel(logging.DEBUG)
+    logging.getLogger('__main__').setLevel(logging.DEBUG)
+
+    MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+    self.tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    self.config = AutoConfig.from_pretrained(MODEL)
+    self.model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+  def predict(self, text):
+    encoded_input = self.tokenizer(text, return_tensors='pt')
+    output = self.model(**encoded_input)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+
+    ranking = np.argsort(scores)
+    ranking = ranking[::-1]
+    sentiments = {}
+    for i in range(scores.shape[0]):
+      l = self.config.id2label[ranking[i]]
+      s = scores[ranking[i]]
+      sentiments[l] = np.round(float(s), 4)
+
+    logging.warning(f"Sentiments for this text file: {sentiments}")
+    return sentiments
+
+  def process_message(self, connector, host, secret_key, resource, parameters):
+    # Process the file and upload the results
+    # host = 'http://host.docker.internal'
+
+    logger = logging.getLogger(__name__)
+    inputfile = resource["local_paths"][0]
+    file_id = resource['id']
+
+    # These process messages will appear in the Clowder UI under Extractions.
+    connector.message_process(resource, "Loading contents of file...")
+
+    # Call actual program
+    # result = subprocess.check_output(['wc', inputfile], stderr=subprocess.STDOUT)
+    # result = result.decode('utf-8')
+    # (lines, words, characters, _) = result.split()
+
+    # PREDICT
+    text = preprocess(inputfile)
+    connector.message_process(resource, f"raw text: {text}")
+    predictions: Dict = self.predict(text)
+    connector.message_process(resource, f"Predictions: {predictions}")
+
+    # Format with "contexts" in extractor_info.json
+    preds = {"predictions": predictions}
+
+    # UPLOAD
+    host = 'http://host.docker.internal'
+    metadata = self.get_metadata(preds, 'file', file_id, host)
+    connector.message_process(resource, f"metadata to upload: {metadata}")
+    pyclowder.files.upload_metadata(connector, host, secret_key, file_id, metadata)
+
+    # logger.debug(metadata)
+
+    # Store results as metadata
+    # result = {'lines': lines, 'words': words, 'characters': characters}
+    # metadata = self.get_metadata(result, 'file', file_id, host)
+
+    # Normal logs will appear in the extractor log, but NOT in the Clowder UI.
+
+    # for i in range(len(predictions)):
+    # Upload metadata to original file
+    # my_metadata = {'Output': predictions}
+
+    # Create Clowder metadata object
+    # host = 'http://host.docker.internal'
+
+
+if __name__ == "__main__":
+  extractor = SingleFileHuggingFace()
+  extractor.start()
